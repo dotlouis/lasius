bab89db7969dabeec6a5dc615ef3272c813c54b1
dot-louis <louis1.lafont@gmail.com>
2015-11-25 10:13:12 +0100

    Added Event Page
    - load event from the previous state if exist, otherwise make the
    request
    - changed eventCard to redirect to the event page

---
+++ www/app/event/event.controller.js
@@ -0,0 +1,33 @@
+angular.module('lasius')
+.controller('event.controller',[
+	'$scope',
+  '$stateParams',
+  '$q',
+  'delayer.service',
+	'Event',
+	function($scope, $stateParams, $q, Delayer, Event){
+
+    $scope.fetchEvent = new Delayer([fetchEvent]);
+
+    $scope.$on('$ionicView.beforeEnter', function(){
+      fetchEvent()
+      .then(function(event){
+        if(event.rrule)
+          event.rruleText = RRule.fromString(event.rrule).toText();
+        $scope.event = event;
+      });
+    });
+
+
+    function fetchEvent(){
+      // if the event is already passed from the previous state
+      // we load it from here
+      if($stateParams.event)
+        return $q.when($stateParams.event);
+      else
+        return Event.get({
+          id: $stateParams.id
+        }).$promise;
+      }
+	}
+]);
