b7456fea73e32dcdb10e7c13bbc98f6a51ae4f7f
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-11-27 12:03:12 +0100

    API_PATH set for all authProviders (satellizer)

--- www/app/loopback.config.js
+++ www/app/loopback.config.js
@@ -4,6 +4,5 @@ angular.module('lasius')
     'APP_CONFIG',
     function(LoopBackResourceProvider, APP_CONFIG){
       // Change the URL where to access the LoopBack REST API server
-      // LoopBackResourceProvider.setUrlBase(APP_CONFIG.API_ENDPOINT+'/api/v'+APP_CONFIG.API_VERSION);
       LoopBackResourceProvider.setUrlBase(APP_CONFIG.API_PATH);
 }]);
