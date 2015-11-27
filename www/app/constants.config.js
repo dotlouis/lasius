2cb5e20bf853b39b01b350d661afc452e2ad5c87
dot-louis <louis1.lafont@gmail.com>
2015-11-27 11:44:32 +0100

    API versionning
    Needs API_HOST (localhost),  API_VERSION (1) and API_ENDPOINT (/api).
    Generated API_PATH: localhost/api/v1

--- www/app/constants.config.js
+++ www/app/constants.config.js
@@ -1,7 +1,14 @@
 angular.module('lasius')
-.constant('APP_CONFIG',{
+.constant('APP_CONFIG',(function(){
+  var params = {
     VERSION: '1',
     FB_APP_ID: '1438646329798081',
-    API_ENDPOINT: 'http://localhost:3005'
-    // API_ENDPOINT: 'https://studloop-studant.rhcloud.com'
-});
+    API_HOST: 'http://localhost:3005',
+    // API_HOST: 'https://studloop-studant.rhcloud.com'
+    API_VERSION: '1',
+    API_ENDPOINT: '/api'
+  };
+  params.API_PATH = params.API_HOST+params.API_ENDPOINT+'/v'+params.API_VERSION;
+  console.log(params);
+  return params;
+})());
