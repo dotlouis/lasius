40f925252839c54dd0af41f2c131c2e07404d296
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-11-30 13:54:20 +0100

    Added cachedResource to enable offline caching
    - decorator to enable cachedResource on Loopback Services
    - fetch function now handle the displaying of cache and network requests

--- www/app/loopback.config.js
+++ www/app/loopback.config.js
@@ -1,4 +1,24 @@
 angular.module('lasius')
+// Replace the resource object created by loopback service generator
+// by a cachedResource object to enable offline behavior
+.decorator('LoopBackResource',[
+  '$delegate',
+  '$cachedResource',
+  function($delegate, $cachedResource, APP_CONFIG){
+    return function(url, params, actions) {
+
+      // the only line that changed from the original resource definition
+      // see LoopBackResource in lb-services
+      var resource = $cachedResource(url, url, params, actions);
+
+      resource.prototype.$save = function(success, error) {
+        var result = resource.upsert.call(this, {}, this, success, error);
+        return result.$promise || result;
+      };
+      return resource;
+    };
+  }
+])
 .config([
     'LoopBackResourceProvider',
     'APP_CONFIG',
